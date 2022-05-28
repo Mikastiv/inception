b9fd2b68a8c3be5cbc9b6bb3b025c3344948b9fd
mikastiv <mikastiv@outlook.com>
2022-05-27 22:49:33 -0400

    mariadb container done

--- srcs/mariadb/setup.sh
+++ srcs/mariadb/setup.sh
@@ -5,3 +5,9 @@ sed -Ei '/^(#)?port/c port                    = 3306' /etc/mysql/mariadb.conf.d/
 
 # Remove bind to local host
 sed -i 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# Update SQL script
+sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" ./setup_db.sql
+sed -i "s/MYSQL_USER/$MYSQL_USER/g" ./setup_db.sql
+sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" ./setup_db.sql
+sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" ./setup_db.sql
