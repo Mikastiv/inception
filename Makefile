3f1af3ca4643439e12da6054833ce32c7413449e
mikastiv <mikastiv@outlook.com>
2022-05-29 17:31:31 -0400

    volumes setup

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 all: up
 
 up:
-	docker-compose -f srcs/docker-compose.yml up -d
+	@docker-compose -f srcs/docker-compose.yml up -d
 
 down:
-	docker-compose -f srcs/docker-compose.yml down
+	@docker-compose -f srcs/docker-compose.yml down
 
-.PHONY: all up down
+clean:
+	@docker-compose -f srcs/docker-compose.yml down --rmi all
+
+.PHONY: all up down clean
--- srcs/mariadb/setup.sh
+++ srcs/mariadb/setup.sh
@@ -2,6 +2,7 @@
 
 # Set the port
 sed -Ei '/^(#)?port/c port                    = 3306' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -Ei "/^datadir/c datadir                 = $MYSQL_DATADIR" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Remove bind to local host
 sed -i 's/^bind-address/#bind-address/' /etc/mysql/mariadb.conf.d/50-server.cnf
@@ -11,3 +12,7 @@ sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" ./setup_db.sql
 sed -i "s/MYSQL_USER/$MYSQL_USER/g" ./setup_db.sql
 sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" ./setup_db.sql
 sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g" ./setup_db.sql
+
+# Setup database
+service mysql start && mysql -u root < setup_db.sql
+rm setup_db.sql
